eeeb4fb23b6a6d50daf6ba911583591f7fa66837
cevex <cevex.dev@gmail.com>
2021-07-11 00:57:22 +0200

    [healthcheck] Review screen + [pathology] Init services, list & item

--- frontend/kinetic-app/src/app/app-routing.module.tns.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { NativeScriptRouterModule } from '@nativescript/angular';
-import { routes } from '@src/app/app.routes';
-
-@NgModule({
-  imports: [NativeScriptRouterModule.forRoot(routes)],
-  exports: [NativeScriptRouterModule]
-})
-export class AppRoutingModule { }
